aa04749919dc42e60f857a70dc9e8f2ecdb2acd7
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-08-17 21:10:44 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# ./build.sh lambda_lite.cpp
+# ./build.sh lambda_full.cpp
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <source>"
     exit 1
@@ -9,18 +11,20 @@ compiler="g++"
 machine=$($compiler -dumpmachine)
 version=$(git describe --tags --always --dirty="-dev" 2>/dev/null || echo "unknown")
 os_name=$(uname -s)
+mkdir -p build
 case "$os_name" in
 Windows* | MINGW*)
     stack_size="4294967296" # "8388608"
-    $compiler -std=c++20 -O3 $source -o $name-$version-$machine-strint.exe -Wl,--stack,$stack_size -DSTACK_MAX=$stack_size
-    $compiler -std=c++20 -O3 $source -o $name-$version-$machine-gmpint.exe -Wl,--stack,$stack_size -DSTACK_MAX=$stack_size -DUSE_GMP -lgmp
+    $compiler -std=c++20 -O3 $source -o build/$name-$version-$machine-strint.exe -DSTACK_MAX=$stack_size -Wl,--stack,$stack_size || exit 1
+    $compiler -std=c++20 -O3 $source -o build/$name-$version-$machine-gmpint.exe -DSTACK_MAX=$stack_size -Wl,--stack,$stack_size -DUSE_GMP -lgmp || exit 1
     ;;
 Linux*)
     stack_size="8388608" # "4294967296"
-    $compiler -std=c++20 -O3 $source -o $name-$version-$machine-strint -Wl,-z,stack-size=$stack_size -DSTACK_MAX=$stack_size
-    $compiler -std=c++20 -O3 $source -o $name-$version-$machine-gmpint -Wl,-z,stack-size=$stack_size -DSTACK_MAX=$stack_size -DUSE_GMP -lgmp
+    $compiler -std=c++20 -O3 $source -o build/$name-$version-$machine-strint -DSTACK_MAX=$stack_size -Wl,-z,stack-size=$stack_size -DSTACK_MAX=$stack_size || exit 1
+    $compiler -std=c++20 -O3 $source -o build/$name-$version-$machine-gmpint -DSTACK_MAX=$stack_size -Wl,-z,stack-size=$stack_size -DSTACK_MAX=$stack_size -DUSE_GMP -lgmp || exit 1
     ;;
 *)
-    echo "Unsupported OS: $os_name"
+    echo "Unsupported OS: $os_name" >&2
+    exit 1
     ;;
 esac
