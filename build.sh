69b04f485eb18b39ed84fe9369745ae0818c018c
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-08-30 17:15:08 +0800

    Add a test version

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ make SOURCE=lambda_lite.cpp
 make SOURCE=lambda_lite.cpp USE_GMP=1
 make SOURCE=lambda_full.cpp
 make SOURCE=lambda_full.cpp USE_GMP=1
+make SOURCE=lambda_test.cpp
+make SOURCE=lambda_test.cpp USE_GMP=1
